a49d3e778e4f5e0e3477054006362db0e324d5ac
Ryan Sivek <nanowizardry@gmail.com>
2018-10-28 21:18:37 -0400

    add folder utilities

--- test/spec/build_spec.js
+++ test/spec/build_spec.js
@@ -9,29 +9,29 @@ var pool = new MortalBuild.ExecPool();
 
 // Create a build definition object (this one uses GCC to build C++ object files)
 var buildDef = {
-      // File extension for the target file(s) (this will be appended
-      // automatically to the target, don't add it to the TPL string!)
-      ext: '.o',
-      // Command to invoke
-      cmd: 'g++',
-      // Any necessary flags for the command
-      args: {
-        // Optionally, FLAGS can be defined with ALL:, DEV:, and/or
-        // PROD: properties which will cause it to change depending
-        // on whether it's a development or production build.
+  // File extension for the target file(s) (this will be appended
+  // automatically to the target, don't add it to the TPL string!)
+  ext: '.o',
+  // Command to invoke
+  cmd: 'g++',
+  // Any necessary flags for the command
+  args: {
+    // Optionally, FLAGS can be defined with ALL:, DEV:, and/or
+    // PROD: properties which will cause it to change depending
+    // on whether it's a development or production build.
 
-        // These flags will be used for all builds
-        all: '-std=c++14 -Iinclude/',
-        // These will be used only for dev builds
-        dev: '-Wall -Weffc++ -g',
-        // These will be used only for production builds
-        prod: '-O3',
-        // These will be used only for test builds
-        test: '-Itest/include -Idep/Catch2/single_include/'
-      },
-      // ECMAScript template for the build command. Use ${source}
-      // for the source file(s) and ${target} for the target file
-      tpl: '${cmd} ${args} -c -o ${target} ${source}'
+    // These flags will be used for all builds
+    all: '-std=c++14 -Iinclude/',
+    // These will be used only for dev builds
+    dev: '-Wall -Weffc++ -g',
+    // These will be used only for production builds
+    prod: '-O3',
+    // These will be used only for test builds
+    test: '-Itest/include -Idep/Catch2/single_include/'
+  },
+  // ECMAScript template for the build command. Use ${source}
+  // for the source file(s) and ${target} for the target file
+  tpl: '${cmd} ${args} -c -o ${target} ${source}'
 };
 
 
@@ -49,8 +49,6 @@ MortalBuild.build({
   console.error('Oops, something went wrong...', err);
 });
 
-// Run the build! This returns a promise so you can use it to be
-// notified when all build commands are complete
 MortalBuild.build({
   execPool: pool,
   buildDef: buildDef,
@@ -62,3 +60,8 @@ MortalBuild.build({
 }, (err) => {
   console.error('Oops, something went wrong...', err);
 });
+
+MortalBuild.fsUtil.ifNewerInDir(path.join(__dirname, '..', '..', 'lib'),
+  path.join(__dirname, '..', '..', 'test'),
+  () => {console.log('lib is newer'); },
+  () => {console.log('test is newer'); });
